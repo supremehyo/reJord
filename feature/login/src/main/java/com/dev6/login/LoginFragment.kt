8d70970b63d72ea68c888d93c78cd4d31a21961c
ChoiHyoSeok <36808015+supremehyo@users.noreply.github.com>
2022-12-12 18:55:35 +0900

    [feat] 로그인 view ui 작성 중
    1. string 리소스 추가
    2. 네비게이션 그래프 login 추가
    3. 로그인 fragment 추가

---
+++ feature/login/src/main/java/com/dev6/login/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.dev6.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dev6.core.base.BindingFragment
+import com.dev6.login.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BindingFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+    override fun initView() {
+        super.initView()
+    }
+
+    override fun initViewModel() {
+        super.initViewModel()
+    }
+
+    override fun initListener() {
+        super.initListener()
+    }
+
+    override fun afterViewCreated() {
+        super.afterViewCreated()
+    }
+}
